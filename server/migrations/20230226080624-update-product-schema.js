d65de7334dbb3e162c13776d09a5405ef42ee2e7
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-02-26 13:54:16 +0530

    commenting out migrations

--- server/migrations/20230226080624-update-product-schema.js
+++ server/migrations/20230226080624-update-product-schema.js
@@ -1,27 +1,27 @@
-'use strict';
+// 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('products', 'sellerId', {
-      type: Sequelize.UUID, 
-      allowNull: false,
-    });
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     await queryInterface.addColumn('products', 'sellerId', {
+//       type: Sequelize.UUID, 
+//       allowNull: false,
+//     });
+//     /**
+//      * Add altering commands here.
+//      *
+//      * Example:
+//      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+//      */
+//   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('products', 'sellerId');
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
-};
+//   async down (queryInterface, Sequelize) {
+//     await queryInterface.removeColumn('products', 'sellerId');
+//     /**
+//      * Add reverting commands here.
+//      *
+//      * Example:
+//      * await queryInterface.dropTable('users');
+//      */
+//   }
+// };
