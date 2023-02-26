3e4845f84dfa3053b7f09f7e5eac289a4ff32ee3
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-02-26 13:54:39 +0530

    set to random UUID if itemId or sellerId is empty

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 const { product } = require('../models')
 
+const {v4: uuidv4} = require('uuid');
+
 router.get('/', (req, res) => {
     res.json('Hello!');
 });
 
 router.post("/", async (req, res) => {
-    const productData = req.body;
+    let {itemId, sellerId, name, description, price, imageUrl, category} = req.body;
+
+    if (itemId == "") 
+        itemId = uuidv4();
+    if (sellerId == "")
+        sellerId = uuidv4();
+
+    const productData = {itemId, sellerId, name, description, price, imageUrl, category};
     await product.create(productData);
     res.json(productData);
 });
