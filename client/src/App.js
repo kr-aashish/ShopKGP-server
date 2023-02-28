b53e38744a86cac8a08483ac6bb69c3592383dce
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-02-28 12:43:34 +0530

    updating commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,27 @@
 import './styles/App.css';
 import useFetch from './hooks/useFetch';
-import devConfig from './config/dev'
+import devConfig from './config/dev';
 import { useState, useEffect } from 'react';
 
 function App() {
-    const [allProducts, setAllProducts] = useState([]);
-
     const prodApiEndpoint = devConfig.apiEndpoints.product;
     const { data, loading, error } = useFetch(prodApiEndpoint);
+    const [allProducts, setAllProducts] = useState([]);
 
     useEffect(() => {
         if (data) {
             setAllProducts(data);
+            console.log(data);
         }
     }, [data]);
 
     if (loading) {
         return <>Loading...</>
     }
+
     if (error) {
         return <>Error: {error.message}</>
     }
-
-    console.log(allProducts);
 
     return (
         <>
