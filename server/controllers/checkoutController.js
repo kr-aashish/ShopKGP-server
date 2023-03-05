db7963f8fef35931683bc736716450d0d5dc76e0
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-03-05 23:03:39 +0530

    Update checkoutController.js

--- server/controllers/checkoutController.js
+++ server/controllers/checkoutController.js
@@ -1,9 +1,34 @@
 const stripe = require('stripe')('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');
 const getRandomUuid = require('../utils/generateUuid')
 
-const checkoutProduct = (req, res) => {
+const checkoutProduct = async (req, res) => {
     console.log("This is the request", req.body);
-    res.status(200).json({"data": "Sample response"});
+
+    let error, status;
+    
+    try {
+        const {product, token} = req.body;
+
+        const customer = await stripe.customers.create({
+            email: token.email, 
+            source: token.id,
+        }) 
+
+        const key = getRandomUuid();
+
+        const charge = await stripe.charges.create({
+            amount: product.price, 
+            currency: "usd", 
+            customer: customer.id, 
+            receipt
+        })
+    } catch (error) {
+        console.log(error);
+        res.send(500).json({"message": "Internal server error"});
+    }
+
+
+    res.status(200).json({"data": "Sample response"});  
 }
 
 module.exports = {checkoutProduct}
