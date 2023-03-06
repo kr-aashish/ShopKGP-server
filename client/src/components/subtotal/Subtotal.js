b23760dc7523c9fc85f6b144daf0a312c86682db
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-03-06 11:53:49 +0530

    Stripe second draft

--- client/src/components/subtotal/Subtotal.js
+++ client/src/components/subtotal/Subtotal.js
@@ -31,27 +31,16 @@ function ElementTable({ data }) {
     </>
   );
 }
-
-async function handleToken(token, addresses) {
-  const product = {
-    name: "Sample product", 
-    price: 200,
-    description: "This is a sample product"
-  }
-  const response = await axios.post("http://localhost:3000/checkout", {token, product})
-  console.log(response.status);
-}
-
 function Subtotal() {
   const [{ basket }, dispatch] = useStateValue();
 
-  const handleToken = async (token, addresses) => {
+  const handleToken = async (token, address) => {
     const product = {
       name: "Sample product",
       price: 200,
       description: "This is a sample product"
     };
-    const response = await axios.post("http://localhost:3001/checkout", {token, product});
+    const response = await axios.post("http://localhost:3001/checkout", {token, product, address});
     console.log(response.status);
   }
 
@@ -101,52 +90,5 @@ function Subtotal() {
     </Box>
   );
 }
-
-
-// function createData(name, calories, fat, carbs, protein) {
-//   return { name, calories, fat, carbs, protein };
-// }
-
-// const rows = [
-//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-//   createData("Eclair", 262, 16.0, 24, 6.0),
-//   createData("Cupcake", 305, 3.7, 67, 4.3),
-//   createData("Gingerbread", 356, 16.0, 49, 3.9),
-// ];
-
-// export function BasicTable() {
-//   return (
-//     <TableContainer component={Paper}>
-//       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>Dessert (100g serving)</TableCell>
-//             <TableCell align="right">Calories</TableCell>
-//             <TableCell align="right">Fat&nbsp;(g)</TableCell>
-//             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-//             <TableCell align="right">Protein&nbsp;(g)</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//           {rows.map((row) => (
-//             <TableRow
-//               key={row.name}
-//               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-//             >
-//               <TableCell component="th" scope="row">
-//                 {row.name}
-//               </TableCell>
-//               <TableCell align="right">{row.calories}</TableCell>
-//               <TableCell align="right">{row.fat}</TableCell>
-//               <TableCell align="right">{row.carbs}</TableCell>
-//               <TableCell align="right">{row.protein}</TableCell>
-//             </TableRow>
-//           ))}
-//         </TableBody>
-//       </Table>
-//     </TableContainer>
-//   );
-// }
 
 export default Subtotal;
--- server/controllers/checkoutController.js
+++ server/controllers/checkoutController.js
@@ -1,4 +1,5 @@
-const stripe = require('stripe')('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');
+// const stripe = require('stripe')('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');
+const stripe = require('stripe')('sk_test_51MiJLISGFdXiDdCYguaI0zcO6jrZl57oqTrHvJp1noXxE6bBDQl8Jeq2DxHK90bZHDMSmjQfU4byPZdxGbAgZqBq00Kcs4NEME');
 const getRandomUuid = require('../utils/generateUuid')
 
 const checkoutProduct = async (req, res) => {
@@ -7,12 +8,23 @@ const checkoutProduct = async (req, res) => {
     let error, status;
     
     try {
-        const {product, token} = req.body;
+        const { product, token } = req.body;
 
         const customer = await stripe.customers.create({
             email: token.email, 
             source: token.id,
-        }) 
+            shipping: {
+                name: token.card.name,
+                address: {
+                    line1: token.card.address_line1,
+                    line2: token.card.address_line2,
+                    city: token.card.address_city,
+                    state: token.card.address_state,
+                    country: token.card.address_country,
+                    postal_code: token.card.address_zip,
+                }
+            }
+        });
 
         const key = getRandomUuid();
 
@@ -20,15 +32,34 @@ const checkoutProduct = async (req, res) => {
             amount: product.price, 
             currency: "usd", 
             customer: customer.id, 
-            receipt
-        })
+            description: product.description,
+            receipt_email: token.email,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    line1: token.card.address_line1,
+                    line2: token.card.address_line2,
+                    city: token.card.address_city,
+                    state: token.card.address_state,
+                    country: token.card.address_country,
+                    postal_code: token.card.address_zip,
+                }
+            },
+            // metadata: {
+            //     order_id: key
+            // }
+        }, {
+            idempotencyKey: key
+        });
+
+        console.log(charge);
+        console.log("Success!");
     } catch (error) {
-        console.log(error);
-        res.send(500).json({"message": "Internal server error"});
+        console.log("Errro occured", error);
+        // res.sendStatus(500).json({"message": "Internal server error"});
     }
 
-
-    res.status(200).json({"data": "Sample response"});  
+    // res.status(200).json({"data": "Sample response"});  
 }
 
-module.exports = {checkoutProduct}
+module.exports = { checkoutProduct };
