c0c9efba6562af3c4b9b507dd159703548eb71b1
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-03-06 14:08:51 +0530

    updating commit

--- client/src/config/dev.js
+++ client/src/config/dev.js
@@ -1,7 +1,7 @@
 const devConfig = {
     apiEndpoints : {
-        product: "http://localhost:4000/product",
-        productById: (id) => `http://localhost:4000/product/${id}`,
+        product: "http://localhost:4000/product/all",
+        productById: (id) => `http://localhost:4000/product/get/${id}`,
         createProduct: "http://localhost:4000/product/create",
         user: "http://localhost:4000/user",
     }
