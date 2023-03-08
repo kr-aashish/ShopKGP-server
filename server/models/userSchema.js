f2ae3c4fc69848340290c3c0bc8443f5563486a1
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-03-08 20:08:20 +0530

    Update userSchema.js

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,35 +1,59 @@
-module.exports = (sequelize, DataTypes) => {
-    const user = sequelize.define('user', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        }, 
-        email: {
-            type: DataTypes.STRING, 
-            allowNull: false, 
-            primaryKey: true,
-        },
-        userId: {
-            type: DataTypes.UUID, 
-            defaultValue: sequelize.UUID4, 
-        },
-        year: {
-            type: DataTypes.INTEGER, 
-            allowNull: true,
-        }, 
-        password: {
-            type: DataTypes.STRING, 
-            allowNull: false,
-        },
-        department: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        token: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-    });
+const { isEmail, isUUID } = require('validator');
 
-    return user;
-}
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    userId: {
+      type: DataTypes.UUID,
+      defaultValue: sequelize.UUIDV4,
+      primaryKey: true,
+      validate: {
+        isUUID: 4
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [2, 100]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+        isEmail: true
+      }
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [8, 100]
+      }
+    },
+    department: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [2, 50]
+      }
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      }
+    }
+  });
+
+  return user;
+};
