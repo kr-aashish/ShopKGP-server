2ac90d6b27d9ae6a390960bdb3ed565139ccf6c1
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-03-09 17:17:41 +0530

    commenting out mongoDB

--- server/app.js
+++ server/app.js
@@ -1,114 +1,114 @@
-require("dotenv").config();
-require("./db").connect();
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-var cors = require("cors");
+// require("dotenv").config();
+// require("./db").connect();
+// const express = require("express");
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+// var cors = require("cors");
 
-const User = require("./models/user");
-const productRoute = require("./routes/product");
-const auth = require("./auth");
+// const User = require("./models/user");
+// const productRoute = require("./routes/product");
+// const auth = require("./auth");
 
-const app = express();
+// const app = express();
 
-app.use(cors());
+// app.use(cors());
 
-app.use(express.json({ limit: "50mb" }));
+// app.use(express.json({ limit: "50mb" }));
 
-app.post("/register", async (req, res) => {
-  try {
-    // Get user input
-    const { name, year, email, password, department } = req.body;
+// app.post("/register", async (req, res) => {
+//   try {
+//     // Get user input
+//     const { name, year, email, password, department } = req.body;
 
-    // Validate user input
-    const oldUser = await User.findOne({ email });
+//     // Validate user input
+//     const oldUser = await User.findOne({ email });
 
-    if (oldUser) {
-      return res.status(409).send("User Already Exist. Please Login");
-    }
+//     if (oldUser) {
+//       return res.status(409).send("User Already Exist. Please Login");
+//     }
 
-    //Encrypt user password
-    encryptedPassword = await bcrypt.hash(password, 10);
+//     //Encrypt user password
+//     encryptedPassword = await bcrypt.hash(password, 10);
 
-    // Create user in our database
-    const user = await User.create({
-      name,
-      year,
-      department,
-      email: email.toLowerCase(), // sanitize: convert email to lowercase
-      password: encryptedPassword,
-    });
+//     // Create user in our database
+//     const user = await User.create({
+//       name,
+//       year,
+//       department,
+//       email: email.toLowerCase(), // sanitize: convert email to lowercase
+//       password: encryptedPassword,
+//     });
 
-    // Create token
-    const token = jwt.sign(
-      { user_id: user._id, email },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: "2h",
-      }
-    );
-    // save user token
-    user.token = token;
+//     // Create token
+//     const token = jwt.sign(
+//       { user_id: user._id, email },
+//       process.env.TOKEN_KEY,
+//       {
+//         expiresIn: "2h",
+//       }
+//     );
+//     // save user token
+//     user.token = token;
 
-    // return new user
-    res.status(200).json(user);
-  } catch (err) {
-    console.log(err);
-  }
-});
+//     // return new user
+//     res.status(200).json(user);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
-app.post("/login", async (req, res) => {
-  try {
-    // Get user input
-    const { email, password } = req.body;
+// app.post("/login", async (req, res) => {
+//   try {
+//     // Get user input
+//     const { email, password } = req.body;
 
-    // Validate user input
-    if (!(email && password)) {
-      res.status(400).send("All input is required");
-      return;
-    }
-    // Validate if user exist in our database
-    const user = await User.findOne({ email });
+//     // Validate user input
+//     if (!(email && password)) {
+//       res.status(400).send("All input is required");
+//       return;
+//     }
+//     // Validate if user exist in our database
+//     const user = await User.findOne({ email });
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-      // Create token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
+//     if (user && (await bcrypt.compare(password, user.password))) {
+//       // Create token
+//       const token = jwt.sign(
+//         { user_id: user._id, email },
+//         process.env.TOKEN_KEY,
+//         {
+//           expiresIn: "2h",
+//         }
+//       );
 
-      // save user token
-      user.token = token;
+//       // save user token
+//       user.token = token;
 
-      // user
-      res.status(200).json(user);
-      return;
-    }
-    res.status(400).send("Invalid Credentials");
-  } catch (err) {
-    console.log(err);
-  }
-});
+//       // user
+//       res.status(200).json(user);
+//       return;
+//     }
+//     res.status(400).send("Invalid Credentials");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
-app.get("/welcome", auth, (req, res) => {
-  res.status(200).send("Welcome 🙌 ");
-});
+// app.get("/welcome", auth, (req, res) => {
+//   res.status(200).send("Welcome 🙌 ");
+// });
 
-// app.use("/api/product", auth, productRoute); //verify token before going to productController
+// // app.use("/api/product", auth, productRoute); //verify token before going to productController
 
-// This should be the last route else any after it won't work
-app.use("*", (req, res) => {
-  res.status(404).json({
-    success: "false",
-    message: "Page not found",
-    error: {
-      statusCode: 404,
-      message: "You reached a route that is not defined on this server",
-    },
-  });
-});
+// // This should be the last route else any after it won't work
+// app.use("*", (req, res) => {
+//   res.status(404).json({
+//     success: "false",
+//     message: "Page not found",
+//     error: {
+//       statusCode: 404,
+//       message: "You reached a route that is not defined on this server",
+//     },
+//   });
+// });
 
-module.exports = app;
+// module.exports = app;
